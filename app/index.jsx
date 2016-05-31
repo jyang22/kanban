e9aee484c60ea9f92cc0cd74249ad60191a9cd14
jyang22 <jyang9122@gmail.com>
2016-05-31 00:30:42 -0700

    local_storage

--- app/index.jsx
+++ app/index.jsx
@@ -3,5 +3,10 @@ require('./main.css');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import alt from './libs/alt';
+import persist from './libs/persist';
+import storage from './libs/storage';
+
+persist(alt, storage, 'app');
 
 ReactDOM.render(<App />, document.getElementById('app'));
